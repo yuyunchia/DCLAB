ca7837e14948ca6d7f1d24d852ca47cd98afe2fb
justine2503 <b08901006@ntu.edu.tw>
2022-10-04 17:54:42 +0800

    upadte Rsa256Core.sv

--- lab2/src/Rsa256Core.sv
+++ lab2/src/Rsa256Core.sv
@@ -16,20 +16,36 @@ module Rsa256Core (
 localparam S_IDLE   = 2'd0;
 localparam S_PREP   = 2'd1;
 localparam S_MONT   = 2'd2;
+localparam S_CALC   = 2'd3;
 
 // ===== Output Buffers =====
 logic [255:0] o_a_pow_d_r, o_a_pow_d_w;
 logic o_finished_r, o_finished_w;
 
 // ===== Parameters =====
-localparam BIT = 9'd256
+localparam BIT = 9'd256;
 logic [8:0] M_counter_r, M_counter_w;
 logic [8:0] MP_counter_r, MP_counter_w;
 logic [255:0] MP_temp_r, MP_temp_w; 	// parameter in ModuloProduct
 logic [255:0] MP_r, MP_w; 				// output for ModuloProduct
-logic MA_start_r, MA_start_w; 			// Montgomery Algorithm start
-logic MA_end_r, MA_end_w; 				// Montgomery Algorithm end
 logic [2:0] state_r, state_w;
+
+logic MA_start_r, MA_start_w; 			// Montgomery Algorithm parameter
+logic [255:0] MA_a_r, MA_a_w;
+logic [255:0] MA_b_r, MA_b_w;
+logic MA_end;
+logic [255:0] MA_o;
+
+MontAlg ma1(
+	.i_clk(i_clk),
+	.i_rst(i_rst),
+	.i_MA_start(MA_start_r),
+	.i_n(i_n),
+	.i_MA_a(MA_a_r),
+	.i_MA_b(MA_b_r),
+	.o_MA(MA_o),
+	.o_MA_end(MA_end)
+);
 
 
 // ===== Output Assignments ===== 
@@ -43,7 +59,7 @@ always_comb begin // M_counter
 			M_counter_w = 9'b0;
 		end
 
-		S_MONT: begin
+		S_CALC: begin
 			M_counter_w = (M_counter_r < BIT) ? M_counter_r + 1 : M_counter_r; // 0~255
 		end
 
@@ -83,11 +99,21 @@ always_comb begin //state
 
 		S_MONT: begin
 			if(i_start) state_w = S_PREP;
-			else state_w = (M_counter_r == BIT && o_finished_r == 1'b1) ? S_IDLE : state_r;
+			else state_w = (MA_end == 1) ? S_CALC : state_r;
 		end
 
-		
-		default: state_w = state_r;
+		S_CALC: begin
+			if(i_start) state_w = S_PREP;
+			else begin
+				if(M_counter_r == BIT) state_w = S_IDLE;
+				else if (MA_end == 1 && M_counter_r < BIT) state_w = S_MONT;
+				else state_w = state_r;
+			end
+		end
+
+		default: begin
+			state_w = state_r;
+		end
 
 	endcase
 end
@@ -98,7 +124,7 @@ always_comb begin //o_finished
 			o_finished_w = 1'b0;
 		end
 
-		S_MONT: begin
+		S_CALC: begin
 			o_finished_w = (M_counter_r == BIT) ? 1'b1 : o_finished_r;
 		end
 
@@ -136,72 +162,59 @@ always_comb begin //Montgomery Algorithm
 	case(state_r)
 		S_IDLE: begin
 			MA_start_w = 1'b0;
-			MA_end_w = 1'b0;
-			o_a_pow_d_w = 256'b0;
+			o_a_pow_d_w = 256'b1;
+			MA_a_w = 256'd0;
+			MA_b_w = 256'd0;
 		end
 
 		S_MONT: begin
-			if(M_counter_r != BIT) begin
+			if(M_counter_r < BIT) begin
 				if(i_d[M_counter_r] == 1'b1) begin
+					MA_a_w = o_a_pow_d_r;
+					MA_b_w = MP_r;
 					MA_start_w = 1'b1;
-					MA_end_w = 1'b0; 
-					MontAlg ma1(
-						.i_clk(i_clk),
-						.i_rst(i_rst),
-						.MA_start(MA_start_w),
-						.i_n(i_n),
-						.MA_a(o_a_pow_d_r),
-						.MA_b(MP_r),
-						.MA_o(o_a_pow_d_w),
-						.MA_end(MA_end_w)
-					)
-
-					if(MA_end_w == 1'b1) MA_start_w = 1'b0;
+					
+					if(MA_end == 1'b1) begin
+						MA_start_w = 1'b0;
+						o_a_pow_d_w = MA_o;
+					end
 					else begin
 						MA_start_w = MA_start_r;
-						MA_end_w = MA_end_r;
 						o_a_pow_d_w = o_a_pow_d_r;
 					end
-
 				end
-				else begin
-					o_a_pow_d_w = o_a_pow_d_r;
-					MA_start_w = MA_start_r;
-					MA_end_w = MA_end_r;
-				end
-
-				if (MA_end_w == 1'b1) begin
-					MA_start_w = 1'b1;
-					MA_end_w = 1'b0;
-					MontAlg ma2(
-							.i_clk(i_clk),
-							.i_rst(i_rst),
-							.MA_start(MA_start_w),
-							.i_n(i_n),
-							.MA_a(MP_r),
-							.MA_b(MP_r),
-							.MA_o(MP_w),
-							.MA_end(MA_end_w)
-						)
-					if(MA_end_w == 1'b1) MA_start_w = 1'b0;
-					else begin
-						MA_start_w = MA_start_r;
-						MA_end_w = MA_end_r;
-						MP_w = MP_r;
-					end
-				end
-				else begin
-					MP_w = MP_r;
-					MA_start_w = MA_start_r;
-					MA_end_w = MA_end_r;
-				end
+				
 			end
 
 			else begin 
 				o_a_pow_d_w = o_a_pow_d_r;
+				MA_start_w = MA_start_r;
+				MA_a_w = MA_a_r;
+				MA_b_w = MA_b_r;
+			end
+		end
+
+		S_CALC: begin
+			if(M_counter_r < BIT) begin
+				MA_a_w = MP_r;
+				MA_b_w = MP_r;
+				MA_start_w = 1'b1;
+
+				if(MA_end == 1'b1) begin
+					MA_start_w = 1'b0;
+					MP_w = MA_o;
+				end
+				else begin
+					MA_start_w = MA_start_r;
+					MP_w = MP_r;
+				end
+			end
+
+			else begin
 				MP_w = MP_r;
 				MA_start_w = MA_start_r;
-				MA_end_w = MA_end_r;
+				MA_a_w = MA_a_r;
+				MA_b_w = MA_b_r;
 			end
 		end
 
@@ -209,21 +222,11 @@ always_comb begin //Montgomery Algorithm
 			o_a_pow_d_w = o_a_pow_d_r;
 			MP_w = MP_r;
 			MA_start_w = MA_start_r;
-			MA_end_w = MA_end_r;
+			MA_a_w = MA_a_r;
+			MA_b_w = MA_b_r;
 		end
 
 	endcase
-end
-
-always_comb begin //o_a_pow_d
-	case(state_r) begin
-		S_IDLE: begin
-			o_a_pow_d_w = 256'b1;
-		end
-
-		default: o_a_pow_d_w = o_a_pow_d_r;
-	endcase
-
 end
 
 // ===== Sequential Circuits =====
@@ -237,7 +240,8 @@ always_ff @(posedge i_clk or negedge i_rst) begin
 		MP_temp_r 		<= 256'b0;
 		MP_r 			<= 256'b0;
 		MA_start_r 		<= 256'b0;
-		MA_end_r 		<= 256'b0;
+		MA_a_r			<= 256'b0;
+		MA_b_r 			<= 256'b0;
 	end
 	else begin
 		state_r 		<= state_w;
@@ -247,22 +251,25 @@ always_ff @(posedge i_clk or negedge i_rst) begin
 		MP_counter_r 	<= MP_counter_w;
 		MP_temp_r 		<= MP_temp_w;
 		MP_r 			<= MP_w;
-		MA_start_r 		<= MA_start_r;
-		MA_end_r 		<= MA_end_r;
+		MA_start_r 		<= MA_start_w;
+		MA_a_r			<= MA_a_w;
+		MA_b_r 			<= MA_b_w;
 	end 
+end
 endmodule
 
 
 module MontAlg(
 	input i_clk,
 	input i_rst,
-	input MA_start, 
+	input i_MA_start, 
 	input [255:0] i_n, 
-	input [255:0] MA_a, 
-	input [255:0] MA_b, 
-	output [255:0] MA_o,
-	output MA_end
-)
+	input [255:0] i_MA_a, 
+	input [255:0] i_MA_b, 
+	output [255:0] o_MA,
+	output o_MA_end
+);
+
 localparam S_IDLE = 3'd0;
 localparam S_LONE = 3'd1;
 localparam S_LODD = 3'd2;
@@ -270,21 +277,23 @@ localparam S_LSFT = 3'd3;
 localparam S_POST = 3'd4;
 
 localparam BIT = 9'd256;
-local [255:0] MA_o_r, MA_o_w;
-local MA_end_r, MA_end_w;
-local [9:0] counter_r, counter_w;
-local [2:0] state_r, state_w;
-local [255:0] MA_a_r, MA_a_w;
-local [255:0] MA_b_r, MA_b_w;
+logic [255:0] o_MA_r, o_MA_w;
+logic o_MA_end_r, o_MA_end_w;
+logic [9:0] counter_r, counter_w;
+logic [2:0] state_r, state_w;
+//local [255:0] MA_a_r, MA_a_w;
+//local [255:0] MA_b_r, MA_b_w;
 
-assign MA_o = MA_o_r;
-assign MA_end = MA_end_r;
+assign o_MA = o_MA_r;
+assign o_MA_end = o_MA_end_r;
+//assign MA_a_w = MA_a;
+//assign MA_b_w = MA_b;
 
 // ===== Combinational Blocks =====
 always_comb begin //state
 	case(state_r)
 		S_IDLE: begin
-			if(MA_start) state_w = S_LONE;
+			if(i_MA_start) state_w = S_LONE;
 			else state_w = state_r;
 		end
 
@@ -301,7 +310,7 @@ always_comb begin //state
 		end
 
 		S_POST: begin
-			state_w = (MA_end_r == 1'b1) ? S_IDLE : state_r;
+			state_w = (o_MA_end_r == 1'b1) ? S_IDLE : state_r;
 		end
 
 		default: state_w = state_r;
@@ -320,38 +329,38 @@ end
 
 always_comb begin //MA_o
 	case (state_r)
-		S_IDLE: MA_o_w = 256'b0;
+		S_IDLE: o_MA_w = 256'b0;
 		S_LONE: begin
-			if(MA_a[counter_r] == 1'b1) MA_o_w = MA_o_r + MA_b;
-			else MA_o_w = MA_o_r;
+			if(i_MA_a[counter_r] == 1'b1) o_MA_w = o_MA_r + i_MA_b;
+			else o_MA_w = o_MA_r;
 		end
 
 		S_LODD: begin
-			if(MA_o_r[0] == 1'b1) MA_o_w = MA_o_r + i_n;
-			else MA_o_w = MA_o_r;
+			if(o_MA_r[0] == 1'b1) o_MA_w = o_MA_r + i_n;
+			else o_MA_w = o_MA_r;
 		end
 
 		S_LSFT: begin
-			MA_o_w = MA_o_r >> 1;
+			o_MA_w = o_MA_r >> 1;
 		end
 
 		S_POST: begin
-			MA_o_w = (MA_o_r >= i_n) ? MA_o_r - i_n : MA_o_r;
+			o_MA_w = (o_MA_r >= i_n) ? o_MA_r - i_n : o_MA_r;
 		end
 
-		default: MA_o_w = MA_o_r;
+		default: o_MA_w = o_MA_r;
 	endcase
 end
 
 always_comb begin //MA_end
 	case(state_r)
-		S_IDLE: MA_end_w = 1'b0;
-		S_POST: MA_end_w = 1'b1;
-		default: MA_end_w = MA_end_r;
+		S_IDLE: o_MA_end_w = 1'b0;
+		S_POST: o_MA_end_w = 1'b1;
+		default: o_MA_end_w = o_MA_end_r;
 	endcase
 end
 
-always_comb begin //MA_a, MA_b
+/*always_comb begin //MA_a, MA_b
 	case(state_r)
 		S_IDLE: begin
 			MA_a_w = MA_a;
@@ -362,25 +371,25 @@ always_comb begin //MA_a, MA_b
 			MA_b_w = MA_b_r;
 		end
 	endcase
-end
+end*/
 
 // ===== Sequential Blocks =====
 always_ff @(posedge i_clk) begin
 	if(i_rst) begin
 		state_r 	<= S_IDLE;
-		MA_o_r 		<= 256'b0;
-		MA_end_r 	<= 1'b0;
+		o_MA_r 		<= 256'b0;
+		o_MA_end_r 	<= 1'b0;
 		counter_r 	<= 9'b0;
-		MA_a_r 		<= MA_a;
-		MA_b_r 		<= MA_b;
+		//MA_a_r 		<= MA_a;
+		//MA_b_r 		<= MA_b;
 	end
 	else begin
 		state_r 	<= state_w;
-		MA_o_r 		<= MA_o_w;
-		MA_end_r 	<= MA_end_w;
+		o_MA_r 		<= o_MA_w;
+		o_MA_end_r 	<= o_MA_end_w;
 		counter_r 	<= counter_w;
-		MA_a_r 		<= MA_a_w;
-		MA_b_r 		<= MA_b_w;
+		//MA_a_r 		<= MA_a_w;
+		//MA_b_r 		<= MA_b_w;
 	end
 	
 end
