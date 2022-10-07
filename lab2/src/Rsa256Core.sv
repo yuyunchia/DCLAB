3a27c50e54fc55cb443fe5c14ec05caeb2cbf8fe
justine2503 <b08901006@ntu.edu.tw>
2022-10-07 19:16:07 +0800

    100722_1915_Justine_modified

--- lab2/src/Rsa256Core.sv
+++ lab2/src/Rsa256Core.sv
@@ -399,20 +399,20 @@ module ModProd (
 	output 			o_MP_end
 );
 
-localparam S_IDLE = 2'd0;
-localparam S_SHFT = 2'd1;
-localparam S_CIRC = 2'd2;
-localparam S_LAST = 2'd3;
+localparam S_IDLE = 4'd0;
+localparam S_SHFT = 4'd1;
+localparam S_CIRC = 4'd2;
+localparam S_MINS = 4'd3;
+localparam S_LAST = 4'd4;
 
 localparam 		BIT = 9'd256;
-logic [2:0] 	state_r, state_w;
+logic [3:0] 	state_r, state_w;
 logic [269:0] 	MP_a_w, MP_a_r;
 logic [9:0] 	counter_w, counter_r;
 logic [269:0] 	MP_out_w, MP_out_r;
 logic 			MP_end_w, MP_end_r;
 
 
-assign MP_a_w = i_MP_a;
 assign o_MP_a = MP_out_r;
 assign o_MP_end = MP_end_r;
 
@@ -442,11 +442,15 @@ always_comb begin //state
 		end
 
 		S_CIRC: begin
-			state_w = (counter_r == BIT) ? S_LAST : S_SHFT;
+			state_w = (counter_r == BIT) ? S_MINS : S_SHFT;
+		end
+
+		S_MINS: begin
+			state_w = S_LAST;
 		end
 
 		S_LAST: begin
-			state_w = (MP_end_r == 1'b1) ? S_IDLE : state_r;
+			state_w = S_IDLE;
 		end
 		default: state_w = state_r;
 	endcase
@@ -459,7 +463,7 @@ always_comb begin //MP_end
 		end
 
 		S_LAST: begin
-			MP_end_w = (counter_r == BIT) ? 1'b1 : MP_end_r;
+			MP_end_w = 1'b1 ;
 		end
 
 		default: MP_end_w = MP_end_r;
@@ -480,8 +484,8 @@ always_comb begin //MP_a
 			MP_a_w = (counter_r < BIT && MP_a_r >= i_n) ? MP_a_r - 1 : MP_a_r;
 		end
 
-		S_LAST: begin
-			MP_a_w = (counter_r == BIT && MP_a_r >= i_n) ? MP_a_r - i_n : MP_a_r;
+		S_MINS: begin
+			MP_a_w = (MP_a_r >= i_n) ? MP_a_r - i_n : MP_a_r;
 		end
 
 		default: MP_a_w = MP_a_r;
@@ -491,7 +495,7 @@ end
 always_comb begin //MP_out
 	case(state_r)
 		S_IDLE: begin
-			MP_out_w = 269'd0;
+			MP_out_w = 270'd0;
 		end
 
 		S_LAST: begin
@@ -505,7 +509,7 @@ end
 always_ff @(posedge i_clk or posedge i_rst) begin
 	if(i_rst) begin
 		state_r 		<= S_IDLE;
-		MP_out_r 		<= 269'b0;
+		MP_out_r 		<= 270'b0;
 		MP_end_r 		<= 1'b0;
 		counter_r 		<= 9'b0;
 		MP_a_r 			<= i_MP_a;
