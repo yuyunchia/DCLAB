0898c3dcd165f6a0181336bcd5b0439a4902a323
yuyunchia <b08901074@ntu.edu.tw>
2022-10-31 23:31:21 +0800

    add framework for Top.sv but not completed

--- lab3/src/Top_original.sv
+++ lab3/src/Top_original.sv
@@ -1,9 +1,9 @@
 module Top (
 	input i_rst_n,
 	input i_clk,
-	input i_key_0, // start
-	input i_key_1, // stop
-	input i_key_2, // pause
+	input i_key_0,
+	input i_key_1,
+	input i_key_2,
 	// input [3:0] i_speed, // design how user can decide mode on your own
 	
 	// AudDSP and SRAM
@@ -46,36 +46,16 @@ module Top (
 );
 
 // design the FSM and states as you like
-localparam S_IDLE       = 0;
-localparam S_I2C        = 1;
-localparam S_RECD       = 2;
-// parameter S_RECD_PAUSE = 3;
-localparam S_PLAY       = 4;
-// parameter S_PLAY_PAUSE = 5;
+parameter S_IDLE       = 0;
+parameter S_I2C        = 1;
+parameter S_RECD       = 2;
+parameter S_RECD_PAUSE = 3;
+parameter S_PLAY       = 4;
+parameter S_PLAY_PAUSE = 5;
 
-logic i2c_oen, i2c_sdat, i2c_finished;
+logic i2c_oen, i2c_sdat;
 logic [19:0] addr_record, addr_play;
 logic [15:0] data_record, data_play, dac_data;
-
-//////// self add wire & register
-logic [3:0] state_w, state_r;
-logic i2c_start_w, i2c_start_r;
-
-logic recorder_start_w, recorder_start_r;
-logic recorder_pause_w, recorder_pause_r;
-logic recorder_stop_w, recorder_stop_r;
-logic recorder_rst_w, recorder_rst_r;
-
-logic player_start_w, player_start_r;
-logic player_pause_w, player_pause_r;
-logic player_stop_w, player_stop_r;
-logic player_rst_w, player_rst_r;
-
-logic switch_w, switch_r; // switch for RECD:0/PLAY:1
-
-
-
-/////////////////////////////////////
 
 assign io_I2C_SDAT = (i2c_oen) ? i2c_sdat : 1'bz;
 
@@ -97,8 +77,8 @@ assign o_SRAM_UB_N = 1'b0;
 I2cInitializer init0(
 	.i_rst_n(i_rst_n),
 	.i_clk(i_clk_100K),
-	.i_start(i2c_start_r),
-	.o_finished(i2c_finished),
+	.i_start(),
+	.o_finished(),
 	.o_sclk(o_I2C_SCLK),
 	.o_sdat(i2c_sdat),
 	.o_oen(i2c_oen) // you are outputing (you are not outputing only when you are "ack"ing.)
@@ -140,77 +120,19 @@ AudRecorder recorder0(
 	.i_rst_n(i_rst_n), 
 	.i_clk(i_AUD_BCLK),
 	.i_lrc(i_AUD_ADCLRCK),
-	.i_start(recorder_start_r),
-	.i_pause(recorder_pause_r),
-	.i_stop(recorder_stop_r),
+	.i_start(),
+	.i_pause(),
+	.i_stop(),
 	.i_data(i_AUD_ADCDAT),
 	.o_address(addr_record),
 	.o_data(data_record),
 );
 
-//////////////////////////////////// Combinational logic //////////////////////////
-
-always_comb begin // state
-    case(state_r)
-    S_IDLE: state_w = S_I2C;
-    S_I2C: begin
-        if(i2c_finished == 1'd1) begin
-			if(switch_r) state_w = S_PLAY;
-			else state_w = S_RECD;
-		end
-    end
-    S_RECD: begin
-		if(switch_r) state_w = S_PLAY;
-		else state_w = S_RECD;
-	end
-	S_PLAY: begin
-		if(switch_r) state_w = S_PLAY;
-		else state_w = S_RECD;
-	end
-    default: state_w = state_r;
-    endcase
+always_comb begin
+	// design your control here
 end
 
-
-always_comb begin // i2c_start
-	case(state_r)
-	S_I2C: i2c_start_w = 1'd1;
-	default: i2c_start_w = 1'd0;
-	endcase
-end
-
-always_comb begin // control for RECD (switch_r == 0)
-	recorder_start_w = 1'b0;
-	recorder_pause_w = 1'b0;
-	recorder_stop_w  = 1'b0;
-	recorder_rst_w   = recorder_rst_r;
-	case(state_r)
-	S_RECD: begin
-		if(!recorder_rst_r) recorder_rst_w = 1'd1;
-		else if(i_key_0) recorder_start_w = 1'd1;
-		else if(i_key_1) recorder_stop_w = 1'd1;
-		else if(i_key_2) recorder_pause_w = 1'd1;
-	end
-	S_PLAY: if(!switch_r) recorder_rst_w = 1'd0; 
-	endcase
-end
-
-
-always_comb begin // control for PLAY (switch_r == 1)
-	
-end
-
-always_comb begin // speed control for PLAY (switch_r == 1)
-	
-end
-
-
-
-
-
-//////////////////////////////////// Sequential logic //////////////////////////
-
-always_ff @(posedge i_clk or negedge i_rst_n) begin
+always_ff @(posedge i_AUD_BCLK or posedge i_rst_n) begin
 	if (!i_rst_n) begin
 		
 	end
